533873ab7f37ed7fa3848aad0668e1d6dd9180d5
sacolle <pedro.h.b.colle@gmail.com>
2025-11-03 13:20:45 -0300

    adicionado flake a nível de subprojeto

---
+++ dev/star-fletcher/flake.nix
@@ -0,0 +1,47 @@
+{
+    description = "A very basic flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }: 
+    let 
+        system = "x86_64-linux";
+        starpuOverlay = f: p: {
+            StarPU = p.callPackage ./starpu.nix { maxBuffers = 10; };
+        };
+        pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ starpuOverlay ];
+        };
+    in
+    {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                pkg-config
+                hwloc
+                StarPU
+                # project libs
+                criterion
+
+                # for bash to work properlly inside vscode
+                bashInteractive
+                # debug tools
+                gdb
+                gcc
+                valgrind
+            ];
+            # export StarPU and hwloc store locations 
+            # for use in vscode intellisence
+            GCC_STORE_PATH = "${pkgs.gcc}";
+            STARPU_STORE_PATH = "${pkgs.StarPU}";
+            HWLOC_STORE_PATH = "${pkgs.hwloc.dev}";
+
+            shellHook = ''
+                export SHELL=/run/current-system/sw/bin/bash
+                echo Added StarPU, Hwloc and gcc to ENV
+            '';
+        };
+    };
+}
