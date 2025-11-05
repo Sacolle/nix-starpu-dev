12b8834d8df96339575b634f97086cf499a5c974
sacolle <pedro.h.b.colle@gmail.com>
2025-11-05 15:36:03 -0300

    deixado no flake externo a versão default do starpu, assim como ignorado executáveis de saída com a extensão .out

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     let 
         system = "x86_64-linux";
         starpuOverlay = f: p: {
-            StarPU = p.callPackage ./starpu.nix { maxBuffers = 10; };
+            StarPU = p.callPackage ./starpu.nix { };
         };
         pkgs = import nixpkgs {
             inherit system;
