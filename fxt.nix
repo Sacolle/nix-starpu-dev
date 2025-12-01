deb64a57fe80d75edf261397ef65390403995831
sacolle <pedro.h.b.colle@gmail.com>
2025-12-01 15:30:11 -0300

    new derivation

---
+++ fxt.nix
@@ -0,0 +1,54 @@
+# from https://github.com/PhoqueEberlue/nixpkgs/blob/add-starpu/pkgs/by-name/st/starpu/package.nix
+# TODO: adicionar opções de compilação extra, como debug
+# https://files.inria.fr/starpu/doc/starpu.pdf
+{ 
+      # derivation dependencies
+    lib,
+    fetchurl,
+    stdenv,
+    autoreconfHook,
+
+    perl,
+    help2man,
+
+    static ? false
+}:
+stdenv.mkDerivation (finalAttrs: {
+    pname = "fxt";
+    system = "x86_64-linux";
+    version = "0.3.14";
+
+    inherit static;
+
+    src = fetchurl {
+        url = "https://download.savannah.gnu.org/releases/fkt/fxt-${version}.tar.gz";
+        hash = "";
+    };
+
+    nativeBuildInputs = [
+        perl
+        help2man
+    ];
+
+    buildInputs = [
+        perl
+        help2man
+    ];
+
+    configureFlags = (lib.optional finalAttrs.static ["CFLAGS=-fPIC" "--enable-static=yes" "--enable-shared=no"]); 
+
+
+      # No need to add flags for CUDA, it should be detected by ./configure
+
+      postConfigure = ''
+        # Patch shebangs recursively because a lot of scripts are used
+        shopt -s globstar
+        patchShebangs --build **/*.sh
+      '';
+
+      enableParallelBuilding = true;
+      doCheck = true;
+})
+
+
+
