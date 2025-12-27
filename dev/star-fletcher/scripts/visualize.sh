e5d37e0c5e14ae48a2931738b68a35aa865cd4fc
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2025-12-27 17:23:14 -0300

    convert to PS and then to PDF. Each page is a frame.

--- dev/star-fletcher/scripts/visualize.sh
+++ dev/star-fletcher/scripts/visualize.sh
@@ -13,6 +13,7 @@ if file $1 | grep 'ASCII text';then
   source $1
   f2=$((n2 / 2))
   [[ $n1 -gt 0 && $n2 -gt 0 && $n3 -gt 0 && $n4 -gt 0 ]] && sfwindow \
-	  < $1 f2=$f2 n2=1 | sfgrey gainpanel=all wantframenum=no polarity=y | sfpen
+								< $1 f2=$f2 n2=1 | sfgrey gainpanel=all wantframenum=no polarity=y | pspen > output.ps
+  convert output.ps output.pdf
 
 fi
