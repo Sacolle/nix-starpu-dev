54287b978d7acb68b235de7b0da06f8d110c3b3a
sacolle <pedro.h.b.colle@gmail.com>
2026-01-08 16:03:56 -0300

    mudado o default de double para float pois o fletcher base é em float

--- dev/star-fletcher/src/includes/floatingpoint.h
+++ dev/star-fletcher/src/includes/floatingpoint.h
@@ -7,23 +7,24 @@
 // Choose precision type with -DFP_FLOAT or -DFP_LONG_DOUBLE (default = double)
 
 // --- Type selection ---
-#if defined(FP_FLOAT)
-    typedef float FP;
-    #define FP_SQRT  sqrtf
-    #define FP_EXP   expf
-    #define FP_SIN   sinf
-    #define FP_COS   cosf
-    #define FP_TAN   tanf
-    #define FP_ATAN  atanf
-    #define FP_LOG   logf
-    #define FP_POW   powf
-    #define FP_CEIL  ceilf
-    #define FP_MIN   fminf
-    #define FP_MAX   fmaxf
-    #define FP_ABS   fabsf
-    #define FP_ARG   ARG_f32
-    #define FP_RAND()  ((float) rand() / (float) RAND_MAX)
-    #define FP_LIT(x) x##f
+#if defined(FP_DOUBLE)
+    typedef double FP;
+    #define FP_SQRT  sqrt
+    #define FP_EXP   exp
+    #define FP_SIN   sin
+    #define FP_COS   cos
+    #define FP_TAN   tan
+    #define FP_ATAN  atan
+    #define FP_LOG   log
+    #define FP_POW   pow
+    #define FP_CEIL  ceil
+    #define FP_MIN   fmin
+    #define FP_MAX   fmax
+    #define FP_ABS   fabs
+    #define FP_ARG   ARG_f64
+    // essa implementação de rand para double é meio ruim, melhorar TODO:
+    #define FP_RAND()  ((double) rand() / (double) RAND_MAX)
+    #define FP_LIT(x) x
 
 #elif defined(FP_LONG_DOUBLE)
     typedef long double FP;
@@ -44,24 +45,23 @@
     #define FP_LIT(x) x##L
 
 #else
-    // Default: double precision
-    typedef double FP;
-    #define FP_SQRT  sqrt
-    #define FP_EXP   exp
-    #define FP_SIN   sin
-    #define FP_COS   cos
-    #define FP_TAN   tan
-    #define FP_ATAN  atan
-    #define FP_LOG   log
-    #define FP_POW   pow
-    #define FP_CEIL  ceil
-    #define FP_MIN   fmin
-    #define FP_MAX   fmax
-    #define FP_ABS   fabs
-    #define FP_ARG   ARG_f64
-    // essa implementação de rand para double é meio ruim, melhorar TODO:
-    #define FP_RAND()  ((double) rand() / (double) RAND_MAX)
-    #define FP_LIT(x) x
+    //defaults to float
+    typedef float FP;
+    #define FP_SQRT  sqrtf
+    #define FP_EXP   expf
+    #define FP_SIN   sinf
+    #define FP_COS   cosf
+    #define FP_TAN   tanf
+    #define FP_ATAN  atanf
+    #define FP_LOG   logf
+    #define FP_POW   powf
+    #define FP_CEIL  ceilf
+    #define FP_MIN   fminf
+    #define FP_MAX   fmaxf
+    #define FP_ABS   fabsf
+    #define FP_ARG   ARG_f32
+    #define FP_RAND()  ((float) rand() / (float) RAND_MAX)
+    #define FP_LIT(x) x##f
 #endif
 
 #endif
