48662c799ab43b44021e86c0402be826b5b26733
sacolle <pedro.h.b.colle@gmail.com>
2026-02-11 14:56:25 -0300

    files

--- dev/star-fletcher/flake.nix
+++ dev/star-fletcher/flake.nix
@@ -2,72 +2,52 @@
     description = "A very basic flake";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-        madagascar.url = "github:Sacolle/nix-madagascar";
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     };
 
-    outputs = { self, nixpkgs, madagascar }: 
+    outputs = { self, nixpkgs }: 
     let 
         system = "x86_64-linux";
+        starpuOverlay = f: p: {
+            StarPU = p.callPackage ../../starpu.nix { enableCUDA = true; };
+        };
         fxtOverlay = f: p: {
             fxt = p.callPackage ../../fxt.nix { static = true; };
         };
         pkgs = import nixpkgs {
             inherit system;
-            overlays = [ fxtOverlay ];
+            overlays = [ starpuOverlay fxtOverlay ];
+            config = { 
+                allowUnfree = true;
+                cudaSupport = true;
+                cudaVersion = "13";
+            };
         };
-        StarPU = pkgs.callPackage ../../starpu.nix { 
-            maxBuffers = 56; 
-            enableTrace = true; 
-        };
-        StarPURelease = pkgs.callPackage ../../starpu.nix { 
-            maxBuffers = 56; 
-            buildMode = "release";
-        };
-
-        baseShell = StarPUVersion: extraArgs: pkgs.mkShell ({
+    in
+    {
+        packages.${system}.default = pkgs.StarPU;
+        devShells.${system}.default = pkgs.mkShell {
             buildInputs = with pkgs; [
                 pkg-config
+                StarPU
                 hwloc
-                # StarPU
-                StarPUVersion
-                # project libs
-                criterion
-
                 # for bash to work properlly inside vscode
                 bashInteractive
-                # for fuser
-                psmisc
-                # debug tools
                 gdb
                 gcc
                 valgrind
-                # scripting
-                python313
-                python313Packages.numpy
-
-                #madagascar
-                madagascar.packages.${system}.default
             ];
             # export StarPU and hwloc store locations 
             # for use in vscode intellisence
             GCC_STORE_PATH = "${pkgs.gcc}";
-            STARPU_STORE_PATH = "${StarPUVersion}";
-            CRITERION_STORE_PATH = "${pkgs.criterion.dev}";
+            STARPU_STORE_PATH = "${pkgs.StarPU}";
             HWLOC_STORE_PATH = "${pkgs.hwloc.dev}";
 
             shellHook = ''
                 export SHELL=/run/current-system/sw/bin/bash
-                echo Added StarPU, Hwloc, criterion and gcc to ENV
-                zsh
+                echo Added StarPU, Hwloc and gcc to ENV
+                echo ${pkgs.fxt}
             '';
-        } // extraArgs);
-    in
-    {
-        devShells.${system} = {
-            # default shell, compiles starpu in debug mode
-            default = baseShell StarPU { COMPILE_MODE = "debug"; };
-            release = baseShell StarPURelease { COMPILE_MODE = "release"; };
         };
     };
 }
